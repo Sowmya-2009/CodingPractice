b608daf92fabcccef75d8d0ccc8e7f5cb8352ff2
Sowmya-2009 <sowmyaaa.m@gmail.com>
2026-04-18 20:12:36 +0530

    code to calculate our bmi

---
+++ bmi.py
@@ -0,0 +1,4 @@
+weight_in_kg=float(input("enter the weight in kg"))
+height_in_meter=float(input("enter the height in meter"))
+bmi=weight_in_kg/(height_in_meter*height_in_meter)
+print("your bmi is:",bmi)
