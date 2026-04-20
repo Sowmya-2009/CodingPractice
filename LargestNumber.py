091c3f5f82b64970be11f0bceed3ddd79c4ae3af
Sowmya-2009 <sowmyaaa.m@gmail.com>
2026-04-20 17:55:43 +0530

    printing the largest ofnthe three numbers using only if statement

---
+++ LargestNumber.py
@@ -0,0 +1,10 @@
+a=float(input("enter the first number:"))
+b=float(input("enter the second number:"))
+c=float(input("enter the third number:"))
+if a>=b and a>=c:
+    print(a,"is largest number")
+if b>=a and b>=c:
+    print(b,"is largest number")
+if c>=a and c>=b:
+    print(c,"is largest number")
+    
