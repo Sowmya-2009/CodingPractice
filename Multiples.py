d04c465bffc2d306a842a2932496760cc00d6b89
Sowmya-2009 <sowmyaaa.m@gmail.com>
2026-04-21 19:08:28 +0530

    to find the multiples of a number out od given 5 numbers

---
+++ Multiples.py
@@ -0,0 +1,18 @@
+n=int(input("enter the number:"))
+a=int(input("enter the first number:"))
+b=int(input("enter the second number:"))
+c=int(input("enter the third number:"))
+d=int(input("enter the fourth number:"))
+e=int(input("enter the fifth number:"))
+print("multiples of",n,"are:")
+if a%n==0:
+    print(a)
+if b%n==0:
+    print(b)
+if c%n==0:
+    print(c)
+if d%n==0:
+    print(d)
+if e%n==0:
+    print(e)
+    
