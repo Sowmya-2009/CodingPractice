96656f0e911be41a2e19f988ea913ea5922c1353
Sowmya-2009 <sowmyaaa.m@gmail.com>
2026-05-04 22:13:02 +0530

    making sure pcode does not contain username and matches'Trident111'

---
+++ user1.py
@@ -0,0 +1,7 @@
+uname=input("enter the user name:")
+pcode=input("enter the code:")
+if uname not in pcode and pcode=='Trident111':
+    print("your code is valid to proceed")
+else:
+    print("your code is not valid to proceed")
+    
