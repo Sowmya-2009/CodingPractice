613eb67bbd23be8e1c86528814d57d679e6d51c0
Sowmya-2009 <sowmyaaa.m@gmail.com>
2026-05-19 22:39:34 +0530

    some of the list functions and methods

---
+++ ListMethods.py
@@ -0,0 +1,16 @@
+L = [90, 600, 600, 600, 5, 7]
+print(L.count(600))
+
+l = [1, 60, 30, 70]
+l.sort()
+print(l)
+
+list3 = [1, 60, 30, 70]
+list3.sort(reverse=True)
+print(list3)
+
+list4 = [50, 400, 30]
+print(sorted(list4))
+
+list5 = [50, 400, 30, 90]
+print(sorted(list5, reverse=True))
